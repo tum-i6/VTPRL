0cf930ee8c9bba03fe4f80af327025195caefe06
Mohammadhossein Malmir <hossein.malmir@tum.de>
2024-11-06 22:57:36 +0100

    adding simulator v0.95 adapted for pushing

--- agent/config.py
+++ agent/config.py
@@ -207,8 +207,13 @@ if __name__ == "__main__":
     """
     config = Config()
 
-    #os.path.dirname(os.path.realpath(__file__)) +
-    xml_file = "./simulator/Linux/ManipulatorEnvironment/configuration.xml" # Change the path if needed
+    # Change the path if needed
+    simulator_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/environment/simulator/"
+    simulator_version = 'v0.95'  # 'v0.9'
+    simulator_platform = 'Windows'  # 'Linux'
+    xml_file = simulator_path + simulator_version + "/" + simulator_platform + "/ManipulatorEnvironment/configuration.xml"
 
     # Update the .xml based on the new changes in the Config class #
     update_simulator_configuration(config, xml_file)
+    print("Successfully updated the simulator configuratiuon file:")
+    print(xml_file)
